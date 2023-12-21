d447c2164e371e9adeb5a9b31ede32cdbc2e9133
Anna Chernysheva <dx8565@gmail.com>
2023-12-21 13:02:44 +0500

    L3-end: Attribute binding

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const app = Vue.createApp({
     data() {
         return{
             product: 'Socks',
-            description: 'A warm fuzzy pair of socks.'
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/',
+            description: 'A warm fuzzy pair of socks.',
+            count: 0
         }
     }
 })
