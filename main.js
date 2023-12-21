630697a2674334dece0638c1120a2ccbd4474041
Anna Chernysheva <dx8565@gmail.com>
2023-12-21 16:53:09 +0500

    L4-end: Conditional Rendering

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const app = Vue.createApp({
         return{
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
+            // inStock: false,
+            inventory: 10,
+            onSale: true,
             url: 'https://www.vuemastery.com/',
             description: 'A warm fuzzy pair of socks.',
             count: 0
